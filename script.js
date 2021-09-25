4318b3c4cd46c07eba3c9c1cdb9b9e00ded9468c
Gabriel Marques <gabrieldonascimentoamrques@gmail.com>
2021-09-25 06:20:17 -0300

    Improved style and JS changes

--- script.js
+++ script.js
@@ -1,26 +1,74 @@
-let adultos = document.getElementById('txta')
-let crianças = document.getElementById('txtc')
-let horas = document.getElementById('txth')
-let container = document.getElementById('box')
-
+const homens = document.getElementById("txta");
+const mulheres = document.getElementById("txtm");
+const crianças = document.getElementById("txtc");
+const duracao = document.getElementById("txtd");
+const container = document.getElementById("box");
 
 function calcular() {
-    if (adultos.value.length === 0 || crianças.value.length === 0 || horas.value.length === 0) {
-        alert('Por favor, preencha todos os campos')
-    } else {
-        container.innerHTML = ''
+  if (
+    homens.value.length === 0 ||
+    mulheres.value.length === 0 ||
+    crianças.value.length === 0 ||
+    duracao.value.length === 0
+  ) {
+    alert("Por favor, preencha todos os campos");
+  } else {
+    container.innerHTML = "";
+    const result = `${meatPP()} gramas de carne <br> ${sideDishPP()} gramas de acompanhamento <br> ${drinksPP()} litros de bebida sem àlcool <br> ${BeerPP()} cervejas em lata/long neck<br> ${parseInt(meatPP()/1.2)} gramas de carvão`;
+    container.innerHTML += result;
+    homens.value = "";
+    mulheres.value = "";
+    crianças.value = "";
+    duracao.value = "";
+  }
+}
 
-        const a = Number(adultos.value)
-        const c = Number(crianças.value)
-        const h = Number(horas.value)
-        const carne = a * 0.25 + c * 0.15 * h
-        const cerveja = a * 1.5 * h
-        const bebida = c * 0.5 * h
-        const result = (`<br>Para esse churrasco será necessário: <br>${carne}kg de carne <br>${cerveja} litros de cerveja <br>${bebida} litros de bebida sem álcool`)
-        container.innerHTML += result
-        adultos.value = ''
-        crianças.value = ''
-        horas.value = ''
+function meatPP() {
+  const a = Number(homens.value);
+  const m = Number(mulheres.value);
+  const c = Number(crianças.value);
+  const d = Number(duracao.value);
 
-    }
+  if (d <= 6) {
+    return a * 400 + m * 300 + c * 200;
+  } else {
+    return a * 600 + m * 450 + c * 300;
+  }
+}
+function sideDishPP() {
+  const a = Number(homens.value);
+  const m = Number(mulheres.value);
+  const c = Number(crianças.value);
+  const d = Number(duracao.value);
+
+  if (d <= 6) {
+    return a * 200 + m * 150 + c * 100;
+  } else {
+    return a * 300 + m * 225 + c * 150;
+  }
+}
+
+function drinksPP() {
+  const a = Number(homens.value);
+  const m = Number(mulheres.value);
+  const c = Number(crianças.value);
+  const d = Number(duracao.value);
+
+  if (d <= 6) {
+    return a * 1.5 + m * 1.5 + c * 0.75;
+  } else {
+    return a * 2.25 + m * 2.25 + c * 1.125;
+  }
+}
+
+function BeerPP() {
+  const a = Number(homens.value);
+  const m = Number(mulheres.value);
+  const d = Number(duracao.value);
+
+  if (d <= 6) {
+    return a * 5 + m * 5;
+  } else {
+    return a * 7 + m * 7;
+  }
 }
